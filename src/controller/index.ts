f4b509a776882eae87545e6928e0e0388b5acbe2
fureli <gafureliani@gmail.com>
2023-02-12 02:20:11 +0400

    refactor: support for  react>=16.8.0

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,4 +1,4 @@
-import { useSyncExternalStore } from 'react'
+import { useSyncExternalStore } from 'use-sync-external-store/shim'
 import { getPathFromKey } from "../helpers/getPathFromKey"
 import { setNestedValue } from "../helpers/setNestedValue"
 
