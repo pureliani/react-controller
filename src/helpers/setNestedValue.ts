ce0c93a4bc183a2a80eed3755ba7a0a82c315b8b
Gabriel Pureliani <gabriel_pureliani@epam.com>
2023-02-17 18:36:00 +0400

    fix: types

--- src/helpers/setNestedValue.ts
+++ src/helpers/setNestedValue.ts
@@ -1,19 +1,27 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-type SetNestedValueArgs = {
-  state: any
+type SetNestedValueArgs<State> = {
+  state: State
   path: (string | symbol | number)[]
   value: any
 }
 
-type SetNestedValue = <State>(args: SetNestedValueArgs) => State
+type SetNestedValue = <State>(args: SetNestedValueArgs<State>) => State
 
 export const setNestedValue: SetNestedValue = ({ state, path, value }) => {
-  const clone = structuredClone(state)
-  if(path.length === 0) return structuredClone(value)
+  const clone = structuredClone(state) as typeof state
+  if(path.length === 0) return structuredClone(value) as typeof state
   let current = clone
   path.slice(0,-1).forEach(p=>{
-    current = current[p]
-  })
-  current[path[path.length - 1]] = value
-  return clone
+    current = (<any>current)[p]
+  });
+  (<any>current)[path[path.length - 1]] = value
+  return clone as typeof state
 }
+
+const stateExample = {
+  a: {
+    b: 3
+  }
+}
+
+const result = setNestedValue({state: stateExample, path: ['a', 'b'], value: 144 })
