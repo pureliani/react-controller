1d96151fb21be880842cf4ba98b7adc75cc84b21
Gabriel Pureliani <gabriel_pureliani@epam.com>
2023-03-02 12:36:07 +0400

    fix: setNestedValue

--- src/helpers/setNestedValue.ts
+++ src/helpers/setNestedValue.ts
@@ -7,13 +7,25 @@ type SetNestedValueArgs<State> = {
 
 type SetNestedValue = <State>(args: SetNestedValueArgs<State>) => State
 
+const replaceReference = <S>(state: S): S => {
+  if (typeof state !== 'object') return state
+  if(Array.isArray(state)) return Object.assign([], state)
+  else return Object.assign({}, state)
+}
+
 export const setNestedValue: SetNestedValue = ({ state, path, value }) => {
-  const clone = Object.assign({}, state)
   if(path.length === 0) return value
+  
+  const clone = replaceReference(state)
   let current = clone
+
   path.slice(0,-1).forEach(p=>{
+    (<any>current)[p] = replaceReference((<any>current)[p])
     current = (<any>current)[p]
   });
+
+  (<any>current)[path[path.length - 1]] = replaceReference((<any>current)[path[path.length - 1]]);
   (<any>current)[path[path.length - 1]] = value
+
   return clone
 }
