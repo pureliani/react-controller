eaf3ac8336a4870398e57174531389f8d6740026
Gabriel Pureliani <gafureliani@gmail.com>
2023-03-05 13:02:32 +0400

    fix: broadcast plugin on window load/reload

--- src/plugins/broadcast.ts
+++ src/plugins/broadcast.ts
@@ -1,13 +1,34 @@
 import type { Plugin } from '../types'
 
+type M<S> = {
+  type: 'GET'
+} | {
+  type: 'SET'
+  state: S
+}
+
 export const broadcast: Plugin = (name: string) => (store) => {
   if (typeof window === 'undefined') return
+  type S = ReturnType<typeof store.getState>
   const channel = new BroadcastChannel(name)
+
   store.subscribeChannel((state) => {
-    channel.postMessage(state)
+    channel.postMessage({state, type: 'SET'} as M<S>)
   })
-  channel.onmessage = async (e: MessageEvent<ReturnType<typeof store.getState>>) => {
-    await store.setState(e.data)
-    store.notify(['internal', 'external'])
+
+  channel.onmessage = async (e: MessageEvent<M<S>>) => {
+    if(e.data.type === 'GET') {
+      console.log('Received GET request')
+      channel.postMessage({ state: store.getState(), type: 'SET' } as M<S>)
+    }
+    if(e.data.type === 'SET') {
+      console.log('Received SET request')
+      await store.setState(e.data.state)
+      store.notify(['internal', 'external'])
+    }
   }
+
+  window.addEventListener('load', () => {
+    channel.postMessage({state: undefined, type: 'GET'} as M<S>)
+  })
 }
