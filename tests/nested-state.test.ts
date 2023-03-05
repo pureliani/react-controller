20f99a740a41bbe7a73b03be3e7b7d12c68d0df4
Gabriel Pureliani <gafureliani@gmail.com>
2023-03-06 01:24:18 +0400

    test: referential equalities in nested state

--- tests/nested-state.test.ts
+++ tests/nested-state.test.ts
@@ -161,6 +161,58 @@ test('Callback initializes nested state', async () => {
   expect(result.current[0].a.b).toBe(17)
 })
 
+/*
+  let's say we have a store from which one component is selecting 'state.a.b.c'
+  and another component is selecting 'state.a', in case 'state.a.b' gets updated, 
+  we want to also update (rerender) the component which was selecting 'state.a'
+  since it's on the path to 'state.a.b', we also don't want to rerender anything
+  which is on the different path (has a different selector).
+
+  e.g: let's say we want to replace 'state.a.b.c'
+  to achieve the results we spoke about, we need to replace references to:
+    state
+    state.a
+    state.a.b
+    state.a.b.c
+
+  replacing references for each level will ensure that rerender will be triggered only for
+  the components which are using one of the above listed selectors.
+
+  references to: state.z or state.a.y or state.a.b.t will not be replaced, therefore
+  components which are using these references will not be rerendered.
+*/
+test('Replaces only the references which are on the path to the selected value', async () => {
+  const initialState = { a: { b: { c: [{ d: 1 }] } }, e: { f: { g: [{ h: 2 }] } } }
+  const { useSelector, getState } = create(initialState)
+  const { result, waitForNextUpdate } = renderHook(() => useSelector(state => state.a.b.c[0].d))
+
+  act(() => {
+    result.current[1](5)
+  })
+
+  await waitForNextUpdate()
+
+  // Changes the reference of each object / array on the path to target.
+  // this should happen so that it causes a rerender if some component is selecting
+  // lets say state.a and we are changing state.a.b
+  expect(getState()).not.toBe(initialState)
+  expect(getState().a).not.toBe(initialState.a)
+  expect(getState().a.b).not.toBe(initialState.a.b)
+  expect(getState().a.b.c).not.toBe(initialState.a.b.c)
+  expect(getState().a.b.c[0]).not.toBe(initialState.a.b.c[0])
+  expect(getState().a.b.c[0].d).not.toBe(initialState.a.b.c[0].d)
+
+  // it should not replace other references in the store
+  expect(getState().e).toBe(initialState.e)
+  expect(getState().e.f).toBe(initialState.e.f)
+  expect(getState().e.f.g).toBe(initialState.e.f.g)
+  expect(getState().e.f.g[0]).toBe(initialState.e.f.g[0])
+  expect(getState().e.f.g[0].h).toBe(initialState.e.f.g[0].h)
+
+  expect(getState().a.b.c[0].d).toBe(5)
+  expect(result.current[0]).toBe(5)
+})
+
 //TODO: fix the error 'BroadcastChannel is undefined'
 test('Nested state emits changes on a broadcast channel', async () => {
   const initialState = { a: { b: [{ c: [1_000_000] }] } }
