ae14dadd0ec03237b82c0cca12321e0b4db96e5a
Gabriel Pureliani <gafureliani@gmail.com>
2023-03-11 12:13:27 +0400

    test: ssi

---
+++ tests/server.test.ts
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment node
+ */
+
+// TODO: fix server tests
+
+// import { act } from '@testing-library/react-hooks'
+// import { create } from '../src'
+// test('Initializes state from server', () => {
+//   const initialState = { a: { b: 5 } }
+//   const { initServerState, getState } = create(initialState)
+//   const stateFromServer: typeof initialState = {a: { b: 100 }}
+//   act(() => {
+//     initServerState(stateFromServer)
+//   })
+//   expect(getState()).toStrictEqual(stateFromServer)
+// })
